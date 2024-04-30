fe65cc52a133db441597f12de22fc39dac6fb3f1
Siegfried Hamm <siegfried.hamm@gmail.com>
2024-04-30 06:55:53 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex items-center h-80 justify-center text-4xl font-mono color-red">
+    <h1 class="w-50 hover:animate-spin-slow text-indigo-700">
+    reality
+  </h1>
+  </div>
+</template>
